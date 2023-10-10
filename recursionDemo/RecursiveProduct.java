6f02d8219b493022bbbdcb8c30e6685cc748238d
LITFAMWOKE93 <samatrafton@gmail.com>
2023-10-10 17:52:47 +0300

    linux build files

--- recursionDemo/RecursiveProduct.java
+++ recursionDemo/RecursiveProduct.java
@@ -1,6 +1,17 @@
-package recursionDemo
+package recursionDemo;
 
 public class RecursiveProduct {
+
+    public static int calculateProduct(int[] nums, int index) {
+
+        // At the end of the array, return 1. This is the base case to terminate
+        if (index == nums.length ) {
+            return 1;
+        } else {
+            // For each recursive call, return the current value multiplied by the product of the remaining array value group.
+            return nums[index] * calculateProduct(nums, index + 1);
+        }
+    }
 
     
 }
