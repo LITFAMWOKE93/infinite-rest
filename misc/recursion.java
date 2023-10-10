cb8fa4141df7029a66407c72c8a67bdcae3e6cd2
LITFAMWOKE93 <samatrafton@gmail.com>
2023-10-10 14:34:09 +0300

    recursion commit

---
+++ misc/recursion.java
@@ -0,0 +1,19 @@
+public class FactorialCalculator {
+
+    public static void main(String[] args) {
+        int number = 5;
+        long factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+
+    public static long calculateFactorial(int n) {
+        // Base case: factorial of 0 is 1
+        if (n == 0) {
+            return 1;
+        }
+        // Recursive case: n! = n * (n-1)!
+        else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
---
+++ recursionDemo/RecursiveProduct.java
@@ -0,0 +1,7 @@
+package recursionDemo
+
+public class RecursiveProduct {
+
+    
+}
+
